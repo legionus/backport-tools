05f9273181e8759e934867909495dd894780391b
Jeff Moyer <jmoyer@redhat.com>
2019-10-02 14:18:04 -0400

    Add GPLv2 license boilerplate
    Signed-off-by: Jeff Moyer <jmoyer@redhat.com>

--- find-fixes.c
+++ find-fixes.c
@@ -1,11 +1,24 @@
 /*
- * find-fixes
+ * find-fixes - Find commits which have a "Fixes:" tag for any of the
+ *              commits provided on the commandline.
  *
- * Description: Find commits which have a "Fixes:" tag for any of the commits
- * provided on the commandline.
- *
- * Author: Jeff Moyer <jmoyer@redhat.com>
  * Copyright(C) Red Hat, Inc., 2018, 2019
+ * Author: Jeff Moyer <jmoyer@redhat.com>
+ * License: GPLv2
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
  * TODO:
  * - factor out shared boilerplate between this and order-commits
--- find-missing-commits.c
+++ find-missing-commits.c
@@ -1,12 +1,25 @@
 /*
- * find-missing-commits
+ * find-missing-commits - Given two trees and an optional list of
+ *                        files, determine which commits exist in tree
+ *                        A that don't exist in tree B.
  *
- * Description: Given two trees and an optional list of files, determine
- * which commits exist in tree A that don't exist in tree B.
- *
- * Author: Jeff Moyer <jmoyer@redhat.com>
  * Copyright(C) Red Hat, Inc., 2019
+ * Author: Jeff Moyer <jmoyer@redhat.com>
+ * License: GPLv2
  *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
  * Usage:
  *
